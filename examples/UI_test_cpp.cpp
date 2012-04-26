c10aca2b6bcab6d67f22f8473e04fb02f9f0053f
Mike Long <mikelong2005@gmail.com>
2012-04-26 17:36:04 +0100

    Update the cpp example to use the new fff header

--- examples/UI_test_cpp.cpp
+++ examples/UI_test_cpp.cpp
@@ -3,7 +3,7 @@ extern "C"{
 #include "SYSTEM.h"
 #include "DISPLAY.h"
 }
-#include "../fff.h"
+#include "../fff3.h"
 
 #include <gtest/gtest.h>
 
@@ -30,7 +30,7 @@ public:
 		RESET_FAKES();
 		RESET_HISTORY();
 		// non default init
-		DISPLAY_get_line_capacity_return_val = 2;
+		DISPLAY_get_line_capacity_fake.return_val = 2;
 	}
 };
 
@@ -40,15 +40,15 @@ public:
 TEST_F(UITests, init_will_initialise_display)
 {
 	UI_init();
-	ASSERT_EQ(DISPLAY_init_call_count, 1);
+	ASSERT_EQ(DISPLAY_init_fake.call_count, 1);
 }
 
 TEST_F(UITests, init_will_register_interrupt_gpio2)
 {
 	UI_init();
-    ASSERT_EQ(SYSTEM_register_irq_call_count, 1);
-    ASSERT_EQ((void *)SYSTEM_register_irq_arg0_val, (void *)UI_button_irq_handler);
-    ASSERT_EQ(SYSTEM_register_irq_arg1_val, IRQ_GPIO_2);
+    ASSERT_EQ(SYSTEM_register_irq_fake.call_count, 1);
+    ASSERT_EQ((void *)SYSTEM_register_irq_fake.arg0_val, (void *)UI_button_irq_handler);
+    ASSERT_EQ(SYSTEM_register_irq_fake.arg1_val, IRQ_GPIO_2);
 }
 
 TEST_F(UITests, when_no_irq_then_missed_irq_counter_zero)
@@ -74,26 +74,26 @@ TEST_F(UITests, when_one_irq_and_valid_callback_then_callback_called)
 {
 	UI_register_button_cbk(button_press_cbk);
 	UI_button_irq_handler();
-	ASSERT_EQ(button_press_cbk_call_count, 1);
+	ASSERT_EQ(button_press_cbk_fake.call_count, 1);
 }
 
 TEST_F(UITests, write_line_outputs_lines_to_display)
 {
 	char msg[] = "helloworld";
 	UI_write_line(msg);
-	ASSERT_EQ(DISPLAY_output_call_count, 1);
-	ASSERT_EQ(strncmp(DISPLAY_output_arg0_val, msg, 26), 0);
+	ASSERT_EQ(DISPLAY_output_fake.call_count, 1);
+	ASSERT_EQ(strncmp(DISPLAY_output_fake.arg0_val, msg, 26), 0);
 }
 
 TEST_F(UITests, when_no_empty_lines_write_line_clears_screen_and_outputs_lines_to_display)
 {
-	DISPLAY_get_line_insert_index_return_val = 2;
+	DISPLAY_get_line_insert_index_fake.return_val = 2;
 	char msg[] = "helloworld";
 
 	UI_write_line(msg);
 
-	ASSERT_EQ(DISPLAY_clear_call_count, 1);
-	ASSERT_EQ(DISPLAY_output_call_count, 1);
+	ASSERT_EQ(DISPLAY_clear_fake.call_count, 1);
+	ASSERT_EQ(DISPLAY_output_fake.call_count, 1);
 	// Check the order of the calls:  Don't care about the first two:
 	// DISPLAY_get_line_capacity and DISPLAY_get_line_insert_index
 	ASSERT_EQ(call_history_idx, 4);
@@ -104,12 +104,12 @@ TEST_F(UITests, when_no_empty_lines_write_line_clears_screen_and_outputs_lines_t
 TEST_F(UITests, when_empty_lines_write_line_doesnt_clear_screen)
 {
 	// given
-	DISPLAY_get_line_insert_index_return_val = 1;
+	DISPLAY_get_line_insert_index_fake.return_val = 1;
 	char msg[] = "helloworld";
 	// when
 	UI_write_line(msg);
 	// then
-	ASSERT_EQ(DISPLAY_clear_call_count, 0);
+	ASSERT_EQ(DISPLAY_clear_fake.call_count, 0);
 }
 
 TEST_F(UITests, when_string_longer_than_26_then_truncated_string_output)
@@ -120,7 +120,7 @@ TEST_F(UITests, when_string_longer_than_26_then_truncated_string_output)
 	// when
 	UI_write_line(input);
 	// then
-	ASSERT_EQ(strncmp(expected, DISPLAY_output_arg0_val, 37), 0);
+	ASSERT_EQ(strncmp(expected, DISPLAY_output_fake.arg0_val, 37), 0);
 }
 
 
