2e0bb77ac7a50735d50ee8a92be9bf7eadfd100c
Mike Long <mikelong2005@gmail.com>
2012-05-24 17:47:13 +0800

    Add fakes for common test cases

--- test/global_fakes.h
+++ test/global_fakes.h
@@ -4,7 +4,25 @@
 
 #include "../fff3.h"
 
-void global_void_func(void);
-DECLARE_FAKE_VOID_FUNC0(global_void_func);
+
+////  Imaginary production code header file ///
+void voidfunc1(int);
+void voidfunc2(char, char);
+long longfunc0();
+
+enum MYBOOL { FALSE = 899, TRUE };
+struct MyStruct {
+    int x;
+    int y;
+};
+enum MYBOOL enumfunc();
+struct MyStruct structfunc();
+////  End Imaginary production code header file ///
+
+DECLARE_FAKE_VOID_FUNC1(voidfunc1, int);
+DECLARE_FAKE_VOID_FUNC2(voidfunc2, char, char);
+DECLARE_FAKE_VALUE_FUNC0(long, longfunc0);
+DECLARE_FAKE_VALUE_FUNC0(enum MYBOOL, enumfunc0);
+DECLARE_FAKE_VALUE_FUNC0(struct MyStruct, structfunc0);
 
 #endif /* GLOBAL_FAKES_H_ */
