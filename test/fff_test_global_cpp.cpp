f8af106f319019a699eebe137f11e79024fbb07c
Mike Long <mikelong2005@gmail.com>
2012-05-24 17:51:20 +0800

    Test suite for global fakes from a cpp context

---
+++ test/fff_test_global_cpp.cpp
@@ -0,0 +1,23 @@
+
+extern "C"{
+    #include "global_fakes.h"
+}
+#include <gtest/gtest.h>
+
+DEFINE_FFF_GLOBALS;
+
+class FFFTestSuite: public testing::Test
+{
+public:
+    void SetUp()
+    {
+        RESET_FAKE(voidfunc1);
+        RESET_FAKE(voidfunc2);
+        RESET_FAKE(longfunc0);
+        RESET_HISTORY();
+    }
+};
+
+#include "test_cases.include"
+
+
