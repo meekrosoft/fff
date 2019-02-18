88d194cc1d89ccd6e48bb706becabcd3444e2940
James Fraser <wulfgar.pro@gmail.com>
2019-02-18 23:38:54 +1100

    Fixed matching commands.

--- script.sh
+++ script.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
 fff_len=`cat fff.h | wc -l`
+
 # test/fff_test_c.c
 gcc -E test/fff_test_c.c > out
-comm -13 <(sort < fff.h | uniq) <(sort < out | uniq) > unique_to_out      # strip out fff.h include
-awk 'FNR==NR{a[$1];next}($1 in a){print}'  unique_to_out fff.h > matched  # find unique matches
-grep -Fxf  <(sort < matched | uniq) <(sort < fff.h | uniq) > coverage     # find coverage of matches in fff.h
+comm -13 <(sort < fff.h | uniq) <(sort < out | uniq) > unique_to_out     # strip out fff.h include
+awk 'FNR==NR{a[$1];next}($1 in a){print}' fff.h unique_to_out > matched  # find unique matches
+awk 'FNR==NR{a[$1];next}($1 in a){print}' matched fff.h > coverage       # find coverage of matches in fff.h
 cov_len=`cat coverage | wc -l`
 echo "test/fff_test_c.c coverage:"
 echo "$cov_len / $fff_len" | bc  -l
@@ -13,8 +14,8 @@ echo "$cov_len / $fff_len" | bc  -l
 # test/fff_test_cpp.cpp
 gcc -E test/fff_test_cpp.cpp -I . > out
 comm -13 <(sort < fff.h | uniq) <(sort < out | uniq) > unique_to_out
-awk 'FNR==NR{a[$1];next}($1 in a){print}'  unique_to_out fff.h > matched
-grep -Fxf  <(sort < matched | uniq) <(sort < fff.h | uniq) > coverage
+awk 'FNR==NR{a[$1];next}($1 in a){print}' fff.h unique_to_out > matched
+awk 'FNR==NR{a[$1];next}($1 in a){print}' matched fff.h > coverage
 cov_len_2=`cat coverage | wc -l`
 echo "test/fff_test_cpp.cpp coverage:"
 echo "$cov_len_2 / $fff_len" | bc  -l
